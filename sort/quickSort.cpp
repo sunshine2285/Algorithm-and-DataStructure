632d9e6ce6a6156d643cafcf36c976d8697e6b6e
sunhaojie <sunshine2285@163.com>
2019-10-02 11:38:12 +0800

    forbid commit exe files

---
+++ sort/quickSort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace::std;
+
+void quickSort()
+{
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int num[7] = {4, 7, 1, 3, 8, 2, 11};
+    
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    {
+        cout << num[i] << endl;
+    }
+    
+    return 0;
+}
